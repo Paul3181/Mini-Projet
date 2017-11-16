306fe3fda99b9d5295cc27002cd82a6392b5cf61
Paul3181 <paulderet8@gmail.com>
2017-11-16 21:02:49 +0100

    Ajout formulaire connexion Ajout fonction dao checkUser Ajout servlet login

--- src/main/java/servlet/ApplicationListener.java
+++ src/main/java/servlet/ApplicationListener.java
@@ -39,10 +39,10 @@ public class ApplicationListener implements ServletContextListener {
 		DAO dao = new DAO(DataSourceFactory.getDataSource());
 		try {
 			List<DiscountCode> allCodes = dao.allCodes();
-			Logger.getLogger("DiscountEditor").log(Level.INFO, "Database already exists");
+			Logger.getLogger("login").log(Level.INFO, "Database already exists");
 			result = true;
 		} catch (SQLException ex) {
-			Logger.getLogger("DiscountEditor").log(Level.INFO, "Database does not exist");
+			Logger.getLogger("login").log(Level.INFO, "Database does not exist");
 		}
 		return result;
 	}
@@ -54,17 +54,17 @@ public class ApplicationListener implements ServletContextListener {
 			}
 		};
 		
-		Logger.getLogger("DiscountEditor").log(Level.INFO, "Creating databse from SQL script");
+		Logger.getLogger("login").log(Level.INFO, "Creating databse from SQL script");
 		try {
 			Connection connection = DataSourceFactory.getDataSource().getConnection();
 			int result = ij.runScript(connection, this.getClass().getResourceAsStream("export.sql"), "UTF-8", System.out, "UTF-8");
 			if (result == 0) {
-				Logger.getLogger("DiscountEditor").log(Level.INFO, "Database succesfully created");
+				Logger.getLogger("login").log(Level.INFO, "Database succesfully created");
 			} else {
-				Logger.getLogger("DiscountEditor").log(Level.SEVERE, "Errors creating database");
+				Logger.getLogger("login").log(Level.SEVERE, "Errors creating database");
 			}
 		} catch (UnsupportedEncodingException | SQLException e) {
-			Logger.getLogger("DiscountEditor").log(Level.SEVERE, null, e);
+			Logger.getLogger("login").log(Level.SEVERE, null, e);
 		}
 
 	}
